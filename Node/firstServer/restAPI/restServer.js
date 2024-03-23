72d6fbbc7c655a58a5c5c3cc0345a2db391371e6
kscold <chan6502@gmail.com>
2024-03-23 21:30:40 +0900

    #6 - REST API 구체화 (GET, POST, PUT, DELETE)
    순수 Node.js의 기능만 이용하여 REST API의 기능을 구체화하여 CRUD를 만듬

--- Node/firstServer/restAPI/restServer.js
+++ Node/firstServer/restAPI/restServer.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs').promises; // fs를 기다림
 
+const users = {}; // 유저 데이터 저장용 객체
+
 http.createServer(async (req, res) => {
     try {
         if (req.method === 'GET') {
@@ -19,16 +21,61 @@ http.createServer(async (req, res) => {
                 return res.end(data);
             } else if (req.url === '/users') {
                 res.writeHead(201, {
-                    'Content-Type': 'application/json; charset=utf-8',
+                    'Content-Type': 'application/json; charset=utf-8', // html이 아니라 json형식으로 보내겠다는 뜻임
                 });
                 return res.end(JSON.stringify(users));
             }
 
             try {
-                const data = await fs.readFile(`.${req.url}`);
+                const data = await fs.readFile(`.${req.url}`); // req.url에 걸리지 않으면
                 return res.end(data);
             } catch (err) {
                 // 404 로직
+            }
+        } else if (req.method == 'POST') {
+            if (req.url === '/user') {
+                let body = ' ';
+                req.on('data', (data) => {
+                    // 요청의 body를 stream 형식으로 받음
+                    body += data;
+                });
+
+                return req.on('end', () => {
+                    console.log('POST 본문(Body):', body);
+                    const { name } = JSON.parse(body); // JSON를 객체로 바꾸고 비구조화 할당
+                    const id = Date.now(); // id는 unique하게 현재 시간으롯 러정
+                    users[id] = name; // 객체에 name 데이터를 대입
+                    res.writeHead(201, {
+                        'Content-Type': 'text/plain; charset=utf-8',
+                    });
+                    res.end('ok');
+                });
+            }
+        } else if (req.method == 'PUT') {
+            if (req.url.startsWith('/user/')) {
+                const key = req.url.split('/')[2];
+
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                return req.on('end', () => {
+                    console.log('PUT 본문(Body):', body);
+                    users[key] = JSON.parse(body).name;
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain; charset=utf-8',
+                    });
+                    return res.end('ok');
+                });
+            }
+        } else if (req.method === 'DELETE') {
+            if (req.url.startsWith('/user/')) {
+                const key = req.url.split('/')[2];
+                delete users[key];
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain; charset=utf-8',
+                });
+                return res.end('ok');
             }
         }
         res.writeHead(404);
