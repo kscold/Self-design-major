85fca7aefb727cc8cc16f815901b7699ac6e41ba
kscold <chan6502@gmail.com>
2024-04-02 18:21:59 +0900

    #9 - Feature 및 State로 컴포넌트 분리하기
    일전에 완성된 React Essentials 페이지를 Feature 및 State로 컴포넌트 분리하여 리팩토링함
    이때 Header 컴포넌트 부분의 랜덤 글을 컴포넌트간의 연관관계가 끊어졌으므로 다른 컴포넌트를 호출할 때 랜덤 글 렌더링이 작동하지 않음

--- React/React-Essentials/src/App.jsx
+++ React/React-Essentials/src/App.jsx
@@ -1,73 +1,14 @@
-import CoreConcept from './components/CoreConcept';
+import CoreConcepts from './components/CoreConcepts';
+import Examples from './components/Examples';
 import Header from './components/Header';
-import TabButton from './components/TabButton';
-import { CORE_CONCEPTS, EXAMPLES } from './data';
-import { useState } from 'react';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  const handleSelect = (selectedButton) => {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  };
-
-  console.log('APP COMPONENT EXCUTING');
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concept</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              // ... 확산연사자를 사용해서 객체의 모든 속성을 렌더링시킬 수 있음
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Example</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
-            >
-              State
-            </TabButton>
-          </menu>
-
-          {!selectedTopic ? (
-            <p>Please select a topic</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
-        <h2>Time to get started!</h2>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
