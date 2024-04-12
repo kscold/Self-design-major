d4e407d96bfe7ef655a9274888e639cb9b463c4b
kscold <chan6502@gmail.com>
2024-04-13 02:55:16 +0900

    #10 - Sequelize를 통해 테이블을 정의함
    Sequelize의 orm을 활용하여 mysql의 테이블을 정의하는 실습을 해보았음

--- Node/sequelize/models/index.js
+++ Node/sequelize/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -13,5 +15,14 @@ const sequelize = new Sequelize(
 );
 
 db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.initiate(sequelize);
+Comment.initiate(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
---
+++ Node/sequelize/models/user.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        User.init(
+            // init 메서드에 컬럼을 정의해서 넣을 수 있음
+            {
+                // 시퀄라이즈는 id를 자동으로 넣어주기 때문에 생략이 가능함
+                name: {
+                    type: Sequelize.STRING(20), // VARCHAR가 아니라 STRING임
+                    allowNull: false,
+                    unique: true,
+                },
+                age: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false,
+                },
+                married: {
+                    type: Sequelize.BOOLEAN, // 시퀄라이즈에서는 BOOLEAN mysql에서는 TIMYINT
+                    allowNull: false,
+                },
+                comment: {
+                    type: Sequelize.TEXT,
+                    allowNull: true,
+                },
+                created_at: {
+                    type: Sequelize.DATE, // DATETIME. MYSQL DATE -> Sequelize DateOnly
+                    allowNull: false,
+                    defaultValue: Sequelize.NOW,
+                },
+            },
+            // 모델에 대한 설정
+            {
+                sequelize,
+                timestamps: false, // createdAt, UpdateAt을 잠깐 끄기 위해 false로 설정
+                underscored: false, // 시퀄라이즈의 글자들을 스네이크케이스(_)로 할지 카멜케이스로 할지 설정
+                paranoid: true, // true이면 deletedAt까지 만들어줌 또한 소프트 delete를 가능하게 해줌
+                modelName: 'User', // 모델이름(자바스크립트에서 쓰는 이름)이 단수라면
+                tableName: 'users', // 테이블이름(sql에서 사용하는 이름) 은 복수형으로 만들어줌
+                charset: 'utf8mb4',
+                collate: 'utf8mb4_general_ci',
+            }
+        );
+    }
+
+    static associate(db) {
+        db.User.hasMany(db.Comment, {
+            foreignKey: 'commenter',
+            sourceKey: 'id',
+        });
+    }
+}
+
+module.exports = User;
