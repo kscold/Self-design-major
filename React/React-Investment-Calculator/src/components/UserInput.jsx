009ac15a2e9a1afe72bd49da7d437a1af1d10ea8
kscold <chan6502@gmail.com>
2024-04-20 22:14:28 +0900

    #12 -userInput state를 부모 컴포넌트로 끌어올림
    Result 컴포넌트에서도 사용해야되기 때문에 userInput 컴포넌트로부터 state를 App.jsx로 끌어올려 결과를 Result 컴포넌트에도 공유함

--- React/React-Investment-Calculator/src/components/UserInput.jsx
+++ React/React-Investment-Calculator/src/components/UserInput.jsx
@@ -1,44 +1,26 @@
-import React, { useState } from 'react';
-
-const UserInput = () => {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+const UserInput = ({ onChange, userInput }) => {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
           <input
-            type="number"
+            type="text"
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange('initialInvestment', event.target.value)
+              onChange('initialInvestment', event.target.value)
             }
           />
         </p>
         <p>
           <label>Annual Investment</label>
           <input
-            type="number"
+            type="text"
             required
             value={userInput.annualInvestment}
             onChange={(event) =>
-              handleChange('annualInvestment', event.target.value)
+              onChange('annualInvestment', event.target.value)
             }
           />
         </p>
@@ -47,21 +29,19 @@ const UserInput = () => {
         <p>
           <label>Expected Return</label>
           <input
-            type="number"
+            type="text"
             required
             value={userInput.expectedReturn}
-            onChange={(event) =>
-              handleChange('expectedReturn', event.target.value)
-            }
+            onChange={(event) => onChange('expectedReturn', event.target.value)}
           />
         </p>
         <p>
           <label>Duration</label>
           <input
-            type="number"
+            type="text"
             required
             value={userInput.duration}
-            onChange={(event) => handleChange('duration', event.target.value)}
+            onChange={(event) => onChange('duration', event.target.value)}
           />
         </p>
       </div>
